172babaa46d2e879bffb99443a302a750bb4c645
Morishing362 <morisita611@gmail.com>
2023-11-05 19:38:37 -0600

    tp, tn, bufferline

--- .config/nvim/lua/config/bufferline.lua
+++ .config/nvim/lua/config/bufferline.lua
@@ -13,4 +13,6 @@ require("bufferline").setup {
 }
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
 vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
+vim.keymap.set('n', '<Leader>tn', '<Cmd>BufferLineMoveNext<CR>', {})
+vim.keymap.set('n', '<Leader>tp', '<Cmd>BufferLineMovePrev<CR>', {})
 
